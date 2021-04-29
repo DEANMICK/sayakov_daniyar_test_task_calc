f7f638739eb4c2ba26c8c511ca3502f22608f1a9
DEANMICK <sayakov01@mail.ru>
2021-04-29 09:14:04 +0600

    added function for sum, substraction, division, multiplication

--- Program.cs
+++ Program.cs
@@ -6,7 +6,46 @@ namespace calculator
     {
         static void Main(string[] args)
         {
-            
+            double firstNum = Convert.ToInt32(Console.ReadLine());
+            double secondNum = Convert.ToInt32(Console.ReadLine());
+            string action = Console.ReadLine();
+            if (action.Equals("+"))
+            {
+                Console.WriteLine(firstNum + " " + action + " " + secondNum + " = " + Sum(firstNum, secondNum));
+            }
+            else if (action.Equals("-"))
+            {
+                Console.WriteLine(firstNum + " " + action + " " + secondNum + " = " + Subtraction(firstNum, secondNum));
+            }
+            else if (action.Equals("/"))
+            {
+                Console.WriteLine(firstNum + " " + action + " " + secondNum + " = " + Division(firstNum, secondNum));
+            }
+            else if (action.Equals("*"))
+            {
+                Console.WriteLine(firstNum + " " + action + " " + secondNum + " = " + Multiplication(firstNum, secondNum));
+            }
+            Console.ReadKey();
+        }
+
+        public static double Multiplication(double a, double b)
+        {
+            return a * b;
+        }
+
+        public static double Division(double a, double b)
+        {
+            return a / b;
+        }
+
+        public static double Sum(double a, double b)
+        {
+            return a + b;
+        }
+
+        public static double Subtraction(double a, double b)
+        {
+            return a - b;
         }
     }
 }
